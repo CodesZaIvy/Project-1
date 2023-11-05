9acaf4adbec84a3cab0ccd51af4e235d7f759f67
CodeZaIvy <ivykwammboka@gmail.com>
2023-11-05 11:01:44 +0300

    Implement first servlet
    Implemented servlet interface

---
+++ art-webapp/src/main/java/com/art/Hello.java
@@ -0,0 +1,40 @@
+package com.art;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.*;
+
+public class Hello implements Servlet {
+
+    @Override
+    public void destroy() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getServletInfo() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+
+        PrintWriter print = res.getWriter();
+        print.println("</b>hello world</b>");
+    }
+}    
